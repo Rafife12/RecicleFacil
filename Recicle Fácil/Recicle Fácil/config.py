64784a9d8f844be578d4ec42021e0ea89189211d
Rafael Silva <130752955+Rafife12@users.noreply.github.com>
2025-07-20 20:32:39 -0300

    Delete Recicle Fácil/Recicle Fácil directory

--- Recicle Fácil/Recicle Fácil/config.py
+++
@@ -1,5 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance/usuarios.db')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
